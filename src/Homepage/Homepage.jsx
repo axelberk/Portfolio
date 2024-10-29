4aeecc364ef774cd0af086933b2f37b67f38f574
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-29 20:52:34 +0100

    homepage style cont

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -5,7 +5,10 @@ const Homepage = () => {
         <div className="Homepage">
             <div className='intro-section'>
                 <img className='pfp' src="me.jpg" alt="" />
-                <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem perferendis officiis error iusto praesentium hic.</h2>
+                <div className="intro-text">
+                    <h1>Axel Bergquist, Web Developer</h1>
+                    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem perferendis officiis error iusto praesentium hic.</h2>
+                </div>
             </div>
         </div>
     )
