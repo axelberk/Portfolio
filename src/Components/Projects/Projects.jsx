f6ec6cb6996fb05ff0f485470b933368141145aa
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-31 20:29:08 +0100

    projects component tbcc

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,11 +2,32 @@ import "./Projects.css"
 
 const Projects = () => {
     return (
-        <div>
-            <div className="img-container">
-                <img src="flagproject1.png" alt="" className="project-img"/>
-                <div className="img-overlay"></div>
-            </div>
+        <div className="projects-container">
+            <h1>Projects</h1>
+            <article className="project">
+                <div className="img-container">
+                    <img src="flagproject1.png" alt="" className="project-img"/>
+                    <div className="img-overlay"></div>
+                </div>
+            </article>
+            <article className="project">
+                <div className="img-container">
+                    <img src="flagproject1.png" alt="" className="project-img"/>
+                    <div className="img-overlay"></div>
+                </div>
+            </article>
+            <article className="project">
+                <div className="img-container">
+                    <img src="flagproject1.png" alt="" className="project-img"/>
+                    <div className="img-overlay"></div>
+                </div>
+            </article>
+            <article className="project">
+                <div className="img-container">
+                    <img src="flagproject1.png" alt="" className="project-img"/>
+                    <div className="img-overlay"></div>
+                </div>
+            </article>
         </div>
     )
 }
