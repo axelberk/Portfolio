99b4d6bf43b5e1b526f7378f25163f075564afa6
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-31 21:05:59 +0100

    projects styling

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import "./Projects.css"
+import { Button, Link    } from "@mui/material"
 
 const Projects = () => {
     return (
@@ -9,11 +10,10 @@ const Projects = () => {
                     <img src="flagproject1.png" alt="" className="project-img"/>
                     <div className="img-overlay"></div>
                 </div>
-            </article>
-            <article className="project">
-                <div className="img-container">
-                    <img src="flagproject1.png" alt="" className="project-img"/>
-                    <div className="img-overlay"></div>
+                <div className="project-text">
+                    <h2>Country Facts</h2>
+                    <p>An app that displays all the worlds countries with basic facts.</p>
+                    <Button variant="contained" sx={{width:"200px", backgroundColor:"#E2F1E7", color:"black"}}>View app</Button>
                 </div>
             </article>
             <article className="project">
@@ -21,11 +21,32 @@ const Projects = () => {
                     <img src="flagproject1.png" alt="" className="project-img"/>
                     <div className="img-overlay"></div>
                 </div>
+                <div className="project-text">
+                    <h2>Spotify Clone</h2>
+                    <p>Made with the Spotify API, this app has basic functionality displaying your playlists etc.</p>
+                    <Button variant="contained" sx={{width: "200px", backgroundColor:"#E2F1E7", color:"black"}}>View app</Button>
+                </div>
             </article>
             <article className="project">
                 <div className="img-container">
                     <img src="flagproject1.png" alt="" className="project-img"/>
                     <div className="img-overlay"></div>
+                </div>
+                <div className="project-text">
+                    <h2>Project 3</h2>
+                    <p>Dunno what this is yet.</p>
+                    <Button variant="contained" sx={{width:"200px", backgroundColor:"#E2F1E7", color:"black"}}>View app</Button>
+                </div>
+            </article>
+            <article className="project">
+                <div className="img-container">
+                    <img src="flagproject1.png" alt="" className="project-img"/>
+                    <div className="img-overlay"></div>
+                </div>
+                <div className="project-text">
+                    <h2>Project 4</h2>
+                    <p>Not sure what to put here yet.</p>
+                    <Button variant="contained" sx={{width:"200px", backgroundColor:"#E2F1E7", color:"black"}}>View app</Button>
                 </div>
             </article>
         </div>
