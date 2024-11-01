ff68990c5e7752137abe2f784c2f1280d4b9e71d
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-11-01 18:49:38 +0100

    reformatting

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,7 +3,12 @@ import "./Contact.css"
 const Contact = () => {
     return (
         <div className="Contact">
-            <h3>Contact</h3>
+            <h3>Contact & Links</h3>
+            <div className="ext-links">
+                <a href="https://github.com/axelberk" target="_blank"><img src="github-mark.png" alt=""/></a>
+                <a href="https://www.linkedin.com/in/axel-bergquist-360940114/" target="_blank"><img src="LI-In-Bug.png" alt="" /></a>
+                <a href=""><img src="icon-gmail.png" alt="" /></a>
+            </div>
         </div>
     )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,15 +12,12 @@ const Navbar = () => {
             <a href="#">
                 A.B</a>
             <div className="page-routes">
-                <Link to="home" smooth={true} duration={500} className="link">Home</Link>|
+                <Link to="top" smooth={true} duration={500} className="link" onClick={() => window.scrollTo({top: 0, behavior:"smooth"})}>Home</Link>|
                 <Link to="projects" smooth={true} duration={500} className="link">Projects</Link>|
                 <Link to="contact" smooth={true} duration={500} className="link">Contact</Link>
             </div>
             
-            <div className="ext-links">
-                <a href="https://github.com/axelberk" target="_blank"><img src="github-mark.png" alt=""/></a>
-                <a href="https://www.linkedin.com/in/axel-bergquist-360940114/" target="_blank"><img src="LI-In-Bug.png" alt="" /></a>
-            </div>
+            
             <Routes>
                 <Route path="/" element=""/>
                 <Route path="/projects" element={<Projects/>}/>
