c060ec72f373e8c3ba879d47335c1ed84f74dbe4
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-11-11 16:01:27 +0100

    screens for project imgs

--- src/Components/ProjectDetail/ProjectDetail.jsx
+++ src/Components/ProjectDetail/ProjectDetail.jsx
@@ -1,8 +1,9 @@
 import React from "react"
 import "./ProjectDetail.css"
-import { Button, Link } from "@mui/material"
+import { Button } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import { IPhoneX, MacbookPro } from "react-device-frames";
 
 
     const ProjectDetail = ({title, description, mainImage, secondImage, stack, projectLink, projectGithub}) => {
@@ -21,14 +22,16 @@ import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutl
                 </div>
                 <img src={mainImage} alt={`&{title} main`} className="projectpage-img"/>
                 <div className="s-container">
-                    <img src={secondImage} alt={`${title} secondary`} className="projectpage-s"/>
+                    <IPhoneX style={{width:"200px"}}>
+                        <img src={secondImage} alt={`${title} secondary`} className="projectpage-s"/>
+                    </IPhoneX>
                     <div className="s-text">
                         <p>{description}</p>
                         <p>Made with:</p>
                         <div className="stacks-used">
-                            {stack.map((tech, index) => {
+                            {stack.map((tech, index) => (
                                 <div key={index} className="chip">{tech}</div>
-                            })}
+                            ))}
                         </div>
                         <div className="project-links">
                             <Button href={projectLink} variant="contained" target="_blank" sx={{width:"110px", backgroundColor:"#E2F1E7", color:"black", borderRadius:"10px", display:"flex", justifyContent:"space-between"}}>Try it<i class="fa fa-external-link" aria-hidden="true"></i></Button>
